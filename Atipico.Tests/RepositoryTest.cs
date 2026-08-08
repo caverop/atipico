da28259832a067a880ea6c297d48aa4c152b6e0b
PaulCavero <pdiego777@gmail.com>
2026-08-08 09:20:39 -0400

    fix repository test

--- Atipico.Tests/RepositoryTest.cs
+++ Atipico.Tests/RepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using Moq.EntityFrameworkCore;
 using Atipico.Infraestructure.Persistence.Repositories;
 using Atipico.Infraestructure.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -12,11 +13,11 @@ namespace Atipico.Tests
         {
             var testObject = new TestClass();
 
-            var context = new Mock<AppDbContext>();
+            var context = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
             var dbSetMock = new Mock<DbSet<TestClass>>(); 
 
             context.Setup(x => x.Set<TestClass>()).Returns(dbSetMock.Object);
-            dbSetMock.Setup(x => x.Add(It.IsAny<TestClass>())).Returns(testObject);
+            dbSetMock.Setup(x => x.Add(It.IsAny<TestClass>()));
 
             var repository = new Repository<TestClass>(context.Object);
 
@@ -31,11 +32,11 @@ namespace Atipico.Tests
         {
             var testObject = new TestClass();
 
-            var context = new Mock<AppDbContext>();
+            var context = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
             var dbSetMock = new Mock<DbSet<TestClass>>();
 
             context.Setup(x => x.Set<TestClass>()).Returns(dbSetMock.Object);
-            dbSetMock.Setup(x => x.Remove(It.IsAny<TestClass>())).Returns(testObject);
+            dbSetMock.Setup(x => x.Remove(It.IsAny<TestClass>()));
 
             var repository = new Repository<TestClass>(context.Object);
 
@@ -46,68 +47,61 @@ namespace Atipico.Tests
         }
 
         [Fact]
-        public void Get_TestClassObjectPassed_ProperMethodCalled()
+        public async Task Get_TestClassObjectPassed_ProperMethodCalled()
         {
-            var testObject = new TestClass();
+            var testObject = new TestClass() { id = 1 };
 
-            var context = new Mock<DbContext>();
+            var context = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
             var dbSetMock = new Mock<DbSet<TestClass>>();
 
             context.Setup(x => x.Set<TestClass>()).Returns(dbSetMock.Object);
-            dbSetMock.Setup(x => x.Find(It.IsAny<int>())).Returns(testObject);
+            dbSetMock.Setup(x => x.FindAsync(It.IsAny<object[]>())).ReturnsAsync(testObject);
 
             var repository = new Repository<TestClass>(context.Object);
-            var result = repository.GetAllAsync();
+            var result = await repository.GetByIdAsync(1);
 
             context.Verify(x => x.Set<TestClass>());
-            dbSetMock.Verify(x => x.Find(It.Is<int>(id => id == 1)));
+            dbSetMock.Verify(x => x.FindAsync(It.Is<object[]>(ids => (int)ids[0] == 1)));
 
-            Assert.Equal((IEnumerable<T>?)testObject, result);
-
+            Assert.Equal(testObject, result);
         }
 
         [Fact]
-        public void GetAll_TestClassObjectPassed_ProperMethodCalled()
+        public async Task GetAll_TestClassObjectPassed_ProperMethodCalled()
         {
             var testObject = new TestClass() { id = 1 };
             var testList = new List<TestClass> { testObject };
 
             var dbSetMock = new Mock<DbSet<TestClass>>();
-            dbSetMock.As<IQueryable<TestClass>>().Setup(m => m.Provider).Returns(testList.AsQueryable().Provider);
-            dbSetMock.As<IQueryable<TestClass>>().Setup(m => m.Expression).Returns(testList.AsQueryable().Expression);
-            dbSetMock.As<IQueryable<TestClass>>().Setup(m => m.ElementType).Returns(testList.AsQueryable().ElementType);
-            dbSetMock.As<IQueryable<TestClass>>().Setup(m => m.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+            MoqExtensions.ConfigureMock(dbSetMock, testList);
 
-            var context = new Mock<AppDbContext>();
+            var context = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
             context.Setup(x => x.Set<TestClass>()).Returns(dbSetMock.Object);
 
             var repository = new Repository<TestClass>(context.Object);
 
-            var result = repository.GetAllAsync();
+            var result = await repository.GetAllAsync();
 
-            Assert.Equal(testList, result.Result.ToList());
+            Assert.Equal(testList, result.ToList());
         }
 
         [Fact]
-        public void Find_TestClassObjectPassed_ProperMethodCalled()
+        public async Task Find_TestClassObjectPassed_ProperMethodCalled()
         {
             var testObject = new TestClass() { id = 1 };
             var testList = new List<TestClass> { testObject };
 
             var dbSetMock = new Mock<DbSet<TestClass>>();
-            dbSetMock.As<IQueryable<TestClass>>().Setup(m => m.Provider).Returns(testList.AsQueryable().Provider);
-            dbSetMock.As<IQueryable<TestClass>>().Setup(m => m.Expression).Returns(testList.AsQueryable().Expression);
-            dbSetMock.As<IQueryable<TestClass>>().Setup(m => m.ElementType).Returns(testList.AsQueryable().ElementType);
-            dbSetMock.As<IQueryable<TestClass>>().Setup(m => m.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+            MoqExtensions.ConfigureMock(dbSetMock, testList);
 
-            var context = new Mock<AppDbContext>();
+            var context = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
             context.Setup(x => x.Set<TestClass>()).Returns(dbSetMock.Object);
 
             var repository = new Repository<TestClass>(context.Object);
 
-            var result = repository.FindAsync(t => t.id == 1);
+            var result = await repository.FindAsync(t => t.id == 1);
 
-            Assert.Equal(testList, result.Result.ToList());
+            Assert.Equal(testList, result.ToList());
         }
     }
 }
