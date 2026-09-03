1832594075e4ad25413d17d2273723cba3ec7b70
caverop <cavero.s.paul@gmail.com>
2026-09-03 10:20:30 -0400

    Staging (#9)
    * add new agent
    
    * fix settings
    
    * add memory project
    
    * modify name docs to specs
    
    * update obsidian graph
    
    * fix bug
    
    * add dev agent
    
    * add some modifications dev agent
    
    * fix graph
    
    * add test web project
    
    * add  minimal fix
    
    * add proactividad
    
    * testing modification (#6)
    
    * testing modification
    
    * test modification
    
    ---------
    
    Co-authored-by: PaulCavero <pdiego777@gmail.com>
    
    * Develop (#8)
    
    * testing modification
    
    * test modification
    
    * testing
    
    ---------
    
    Co-authored-by: PaulCavero <pdiego777@gmail.com>
    
    ---------
    
    Co-authored-by: PaulCavero <pdiego777@gmail.com>

--- Atipico.Web.Tests/LoginTests.cs
+++ Atipico.Web.Tests/LoginTests.cs
@@ -16,6 +16,6 @@ public class LoginTests : BunitContext
 
         var encabezado = cut.Find("h1");
 
-        Assert.Equal("ATIPICO3", encabezado.TextContent);
+        Assert.Equal("ATIPICO4", encabezado.TextContent);
     }
 }
